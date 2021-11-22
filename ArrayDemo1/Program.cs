160652644ed51c7d32aa5a1274c0bfca65358957
Tarik <t.rock22@icloud.com>
2021-11-22 14:52:03 -0500

    update 11.22

--- ArrayDemo1/Program.cs
+++ ArrayDemo1/Program.cs
@@ -6,7 +6,47 @@ namespace ArrayDemo1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = new[] { 5, 10, 15, 20, 25 };
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("Now let's make a string array");
+            string[] names = new string[] { "Fred", "Sally", "Julia", "Sam" };
+            Console.WriteLine(names[0]);// print first name
+            Console.WriteLine(names[2]);// print the third name
+
+            //Let's suppose we need to change one
+            //Let's change sam to samantha
+
+            names[3] = "Samantha";
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            //Let's create an array but no initialize it. We do however,
+            // have to state the size when we create it.
+            //We'll create an array with 4 slots, but intially they are empty.
+            // We can manually add names to the slots.
+
+            Console.WriteLine("Create an array without initializing it");
+
+            string[] words = new string[4];
+
+            words[0] = "Apple";
+            words[1] = "Banana";
+            words[2] = "Orange";
+            words[3] = "Watermelon";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+            
+
         }
     }
 }
---
+++ ArrayDemo2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ArrayDemo2
+{
+    class MainClass
+    {
+
+        public static void PrintMenu(string[] theItems, decimal[] thePrices)
+        {
+            Console.WriteLine("Here is our menu!");
+            for (int i = 0; i < theItems.Length; i++)
+            {
+                Console.WriteLine($"{theItems[i],-12}{ thePrices[i],-6}");
+            }
+
+
+        }
+        public static int FindItem(string[] theItems, string search)
+        {
+            for (int i = 0; i < theItems.Length; i++)
+            {
+                if (theItems[i].ToLower() == search.ToLower())
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            //On this second demo we will learn how to write multiple arrays,
+            //creating data
+
+            // creating a menu at a coffee house
+            //Items         price
+            //,,,,,         ......
+            //coffee        2.00
+            //espresso      2.50
+            //muffin        3.50
+            //croissant     4.00
+
+            //
+            //we'll create one array for items, and one array for price
+
+            string[] items = new string[] { "Coffee", "Esspresso", "Muffin", "Croissant" };
+            decimal[] prices = new decimal[] { 2.00m, 2.50m, 3.50m, 4.00m };
+
+            PrintMenu(items, prices);
+
+
+            Console.WriteLine("Please make a selection");
+            string entry = Console.ReadLine();
+            int index = FindItem(items, entry);
+            Console.WriteLine(index);
+
+            if (index > -1)
+            {
+                Console.WriteLine($"The price of {entry[index]} is {prices[index]}");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, we do not have {entry}!");
+            }
+            //
+            //
+            //
+            //
+            //for (int i = 0; i < items.Length; i++)
+            //{
+            //if (items[i] == entry) "original"
+            //if we want to make sure its case sensitive
+            //if (items[i].ToLower() == entry.ToLower())
+            //{
+            //    Console.WriteLine($"The price of {entry} is {prices[i]}");
+            //    break;
+            //}
+
+
+            //}
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
