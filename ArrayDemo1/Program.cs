28c2e8ca231ab59346a07ff805af8a379b2f210f
Tarik <t.rock22@icloud.com>
2021-11-22 13:00:32 -0500

    week2 new repo

---
+++ ArrayDemo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayDemo1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
